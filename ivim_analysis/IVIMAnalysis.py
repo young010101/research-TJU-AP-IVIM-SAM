0a5e0bdbbaa2285efd96d16c804cc843a96d60b4
natr <yc5@tju.edu.cn>
2024-08-26 00:19:23 +0800

    add class paramaps to save time consume result as pickle

--- ivim_analysis/IVIMAnalysis.py
+++ ivim_analysis/IVIMAnalysis.py
@@ -2,6 +2,8 @@ import numpy as np
 import nibabel as nib
 from matplotlib import pyplot as plt
 from math import pi
+import pickle
+import os 
 
 from dipy.reconst.ivim import IvimModel
 from dipy.core.gradients import gradient_table
@@ -13,11 +15,12 @@ class IVIMAnalysis:
     
     Attributes:
     """
-    def __init__(self, nii_path : str, x_roi : int, y_roi : int, rad : int):
+    def __init__(self, nii_path: str, x_roi: int, y_roi: int, rad: int, patient_id: str):
         self.nii_path = nii_path
         self.x_roi = x_roi
         self.y_roi = y_roi
         self.rad = rad
+        self.patient_id = patient_id
         self.img = nib.load(self.nii_path)
         self.img_data = self.img.get_fdata()
         self.mask_roi = None
@@ -25,6 +28,15 @@ class IVIMAnalysis:
         self.bvals =  None
         self.bvecs = None
         self.data_slice = None
+        self.estimated_params_roi = None
+        self.ivim_params_maps = None
+
+    class param_maps:
+        def __init__(self, S0, f, D_star, D):
+            self.S0 = S0
+            self.f = f
+            self.D_star = D_star
+            self.D = D
 
     def preprocess_data(self):
         """!Don't RUN!!! No pass the test
@@ -44,6 +56,11 @@ class IVIMAnalysis:
         # !Only one slice is used for now
         self.data_slice = self.img_data[:,:,pancreas_slice,:]
         self.ivimfit = ivimmodel.fit(self.data_slice)
+        self.ivim_params_maps = self.param_maps(self.ivimfit.S0_predicted, self.ivimfit.perfusion_fraction, self.ivimfit.D_star, self.ivimfit.D)
+
+    def save_ivim_params(self):
+        """Save the IVIM parameters to a pickle file"""
+        pass
 
     def plot_maps(self):
         plot_map(self.ivimfit.S0_predicted, "Predicted S0", (0, 10000), "predicted_S0")
@@ -60,10 +77,13 @@ class IVIMAnalysis:
         plot_map_roi(self.ivimfit.D, "D", (0, 0.001), "diffusion_coeff", self.x_roi, self.y_roi)
 
     def run_analysis(self, bvals, bvecs):
-        self.preprocess_data()
+        # self.preprocess_data()
         self.generate_roi_mask()
         self.fit_ivim_model(bvals, bvecs)  # Consumes a lot of time
         self.plot_maps()
+        
+        # TODO: If `pickle` file found, load the data from it
+        
 
     def plt_circle_roi(self):
         """plot the ROI circle on the image"""
@@ -92,6 +112,26 @@ class IVIMAnalysis:
             intensive_of_10b[i_bval-9] = intense_roi
         
         plt.scatter([0, 20, 50, 80, 150, 200, 500, 800, 1000, 1500],intensive_of_10b)
+
+    def estimated_params_of_roi(self, is_print = False):
+        """List all poisition of where value is true"""
+        estimated_params_roi = []
+
+        shape_ivim_params = self.ivimfit.model_params.shape
+
+        mask_roi = self.mask_roi
+
+        for i in range(shape_ivim_params[2]):
+            estimated_params_roi.append(np.sum(np.nan_to_num(self.ivimfit.model_params[:,:,i])*mask_roi)/mask_roi.sum())
+
+        if is_print:
+            for param in estimated_params_roi:
+                print(f"Estimated parameter: {param}")
+            
+        self.estimated_params_roi = estimated_params_roi
+
+        return estimated_params_roi
+
 
 if __name__ == "__main__":
     nii_path = 'ST0_A01-AB+MRCP+CE-Q_20240120092423_11.nii.gz'
