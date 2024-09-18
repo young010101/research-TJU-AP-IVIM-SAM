ca4c95d6135c4dd5a58b91bfce97c61a319d0a4c
natr <yc5@tju.edu.cn>
2024-09-18 22:54:47 +0800

    add cp IVIM to a folder

--- ivim_analysis/cat_bval.sh
+++ ivim_analysis/cat_bval.sh
@@ -12,7 +12,10 @@ TAG="ivimap_9_patients"
 NIFTI_DIR="${BASE_NIFTI_DIR}/${TAG}/${FIX_CURRENT_DATE}"
 
 # Change to the NIfTI directory
-cd "$NIFTI_DIR" || { echo "NIFTI directory not found!"; exit 1; }
+cd "$NIFTI_DIR" || {
+    echo "NIFTI directory not found!"
+    exit 1
+}
 
 # Base directory for the bval files
 FLAG_STATIC_DIR=true
@@ -29,7 +32,7 @@ LOG_FILE="${OUTPUT_BASE_DIR}/${BASE_LOG_FILE}"
 
 # Clear $OUTPUT_BASE_DIR
 rm -rf "$OUTPUT_BASE_DIR"/*
-echo "Clearing $OUTPUT_BASE_DIR" >> "$LOG_FILE"
+echo "Clearing $OUTPUT_BASE_DIR" >>"$LOG_FILE"
 
 # Loop through each patient's directory
 for patient_id in *; do
@@ -38,13 +41,13 @@ for patient_id in *; do
         echo "Processing directory: $patient_id"
 
         base_filename_1="${patient_id%%-*}"
-        echo "Base filename: $base_filename_1" >> "$LOG_FILE"
+        echo "Base filename: $base_filename_1" >>"$LOG_FILE"
         all_files_less_than_10=true
 
-    # Loop through each .bval file in the patient directory
-    for file in *.bval; do
-        # Check if any .bval files exist
-        if [ ! -e "$file" ]; then
+        # Loop through each .bval file in the patient directory
+        for file in *.bval; do
+            # Check if any .bval files exist
+            if [ ! -e "$file" ]; then
                 echo "No .bval files found in $patient_id"
                 continue
             fi
@@ -53,42 +56,7 @@ for patient_id in *; do
 
             # Extract the base file name without extension
             base_filename="${file%.*}"
-            echo "Base filename: $base_filename" >> "$LOG_FILE"
-
-            # IFS='.' read -r -a array <<< "$file"
-            # echo "IVIM filename: ${array[0]}"
-
-            cat "$file" >> "$LOG_FILE"
-
-            # Split the 1st line of the .bval file by space
-            # Read the first line of the .bval file
-            read -r first_line < "$file"
-
-            # Split the first line into an array
-            IFS=' ' read -r -a bval_array <<< "$first_line"
-            
-            # Output the array size
-            array_size="${#bval_array[@]}"
-            echo "# of b: $array_size" >> "$LOG_FILE"
-
-            # Check if array size >= 10
-            if [ "$array_size" -ge 10 ]; then
-                massage="[v]Array size $array_size is greater than or equal to 10 for file $file in $base_filename_1"
-                echo $massage >> "$LOG_FILE"
-                echo $massage
-                # Since we found a file with array size >=10, set the flag to false
-                all_files_less_than_10=false
-            fi
-
-            # Output the values
-            # echo "Values in the .bval file:"
-            # for value in "${bval_array[@]}"; do
-            #     echo "$value"
-            # done
-
-            # while IFS= read -r line; do
-            #     echo "$line"
-            # done < "$file"
+            echo "Base filename: $base_filename" >>"$LOG_FILE"
 
             # Create the patient's directory in the output location
             patient_output_dir="${OUTPUT_BASE_DIR}/${base_filename_1}"
@@ -104,16 +72,57 @@ for patient_id in *; do
             else
                 echo "Warning: Corresponding .nii.gz file not found for $file"
             fi
+
+            # IFS='.' read -r -a array <<< "$file"
+            # echo "IVIM filename: ${array[0]}"
+
+            cat "$file" >>"$LOG_FILE"
+
+            # Split the 1st line of the .bval file by space
+            # Read the first line of the .bval file
+            read -r first_line <"$file"
+
+            # Split the first line into an array
+            IFS=' ' read -r -a bval_array <<<"$first_line"
+
+            # Output the array size
+            array_size="${#bval_array[@]}"
+            echo "# of b: $array_size" >>"$LOG_FILE"
+
+            # Check if array size >= 10, if so, cp the file to the output directory
+            if [ "$array_size" -ge 10 ]; then
+                massage="[v]Array size $array_size is greater than or equal to 10 for file $file in $base_filename_1"
+                echo $massage >>"$LOG_FILE"
+                echo $massage
+                # Since we found a file with array size >=10, set the flag to false
+                all_files_less_than_10=false
+
+                # Create the patient's directory in the output location
+                IVIM_folder="../static_IVIM"
+                IVIM_OUTPUT_DIR="${OUTPUT_BASE_DIR}/${IVIM_folder}"
+                mkdir -p "$IVIM_OUTPUT_DIR"
+                cp "$nii_file" "${IVIM_OUTPUT_DIR}/${base_filename_1}_${array_size}.nii.gz"
+            fi
+
+            # Output the values
+            # echo "Values in the .bval file:"
+            # for value in "${bval_array[@]}"; do
+            #     echo "$value"
+            # done
+
+            # while IFS= read -r line; do
+            #     echo "$line"
+            # done < "$file"
         done
 
         # After processing all files in patient directory
         if [ "$all_files_less_than_10" = true ]; then
             massage="[x]All files in $base_filename_1 have array sizes less than 10."
-            echo $massage >> "$LOG_FILE"
+            echo $massage >>"$LOG_FILE"
             echo $massage
         fi
 
-        echo "-----------------------------------" >> "$LOG_FILE"
+        echo "-----------------------------------" >>"$LOG_FILE"
         echo "-----------------------------------"
         cd ..
     fi
